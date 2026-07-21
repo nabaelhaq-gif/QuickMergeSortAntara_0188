d6c5f67f576c71398ac2c95663ce85be2736a3b0
nabaelhaq-gif <nabaelhaq@gmail.com>
2026-07-21 14:48:07 +0700

    membuat perulangan integer i

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -22,4 +22,13 @@ void input()
             cout << "\nMaksimal Panjang Array Adalah 20";
         }
     }
+    cout << "\n--------------------------" << endl;
+    cout << "\nInputkan Isi Element Array" << endl;
+    cout << "\n--------------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Array index ke- " << i << " : ";
+        cin >> arr[i];
+    }
 }
