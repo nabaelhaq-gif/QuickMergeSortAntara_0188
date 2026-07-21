03bc8a28bb82f701d30b21b787e0530bde61e572
nabaelhaq-gif <nabaelhaq@gmail.com>
2026-07-21 15:13:29 +0700

    membuat prosedur void output

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -88,3 +88,13 @@ void mergeSort(int low, int high)
         arr[x] = B[x];
     }
 }
+
+void output()
+{
+    cout << "\nData Setelah Diurutkan (Merge Sort): ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
