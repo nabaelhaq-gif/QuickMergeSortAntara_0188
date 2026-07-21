d3662c7d7252a3b9a31b9f158f8daa723c1edeb6
nabaelhaq-gif <nabaelhaq@gmail.com>
2026-07-21 15:29:10 +0700

    membuat prosedur output

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -84,4 +84,17 @@ void quicksort(int low, int high) //membuat prosedur quicksort dengan parameter 
     }
     quicksort(low, j - 1); //pemanggilan rekursive prosedur q_sort untuk mengurutkan sub array sebelah kiri
     quicksort(j + 1, high); //pemanggilan rekursive prosedur q_sort untuk mengurutkan sub array sebelah kanan
+}void output() //pembuatan prosedur output
+{
+    cout << "\n-------------------" << endl; //display untuk menampilkan keterangan elemnt array yang telah di urutkan
+    cout << "\nInputkan Isi elemnt array" << endl;
+    cout << "\n-------------------" << endl;
+
+    for (int i = 0; i < n; i++) //loopin untuk menampilkan elemnt array yang telah di urutkan
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << "\n\nJumlah perbandingan : " << cmp_count << endl; //untuk menampilkan jumlah perbandingan didalam algoritma
+    cout << "Jumlah Pergerakan Data : " << mov_count << endl; //untuk menampilkan jumlah pergerakan data
 }
